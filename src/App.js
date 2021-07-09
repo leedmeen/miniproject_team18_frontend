dad1d4b48763269b67d2021f0988325e5c87ab0b
leedmeen <leedmeen@gmail.com>
2021-07-09 21:37:44 +0900

    button, input, header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Header></Header>
     </React.Fragment>
   );
 }
--- src/element/Input.js
+++ src/element/Input.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = (props) => {
-  const {text, backgroundColor, color, fontSize, fontWeight, border, width, height, margin, padding, top, left} = props;
+const Input = (props) => {
+  const {type, placeholder, color, fontSize, border, width, height, margin, padding, top, left, cols, rows} = props;
   const styles = {
-    backgroundColor: backgroundColor,
+    border: border,
     color: color,
     fontSize: fontSize,
-    fontWeight: fontWeight,
-    border: border,
     width: width,
     height: height,
     margin: margin,
     padding: padding,
     top: top,
     left: left,
+    cols: cols,
+    rows: rows,
   }
 
-  return (
+  return(
     <React.Fragment>
-      <Btn {...styles}>{text}</Btn>
+      <Ipt {...styles} cols={type === 'textarea'? rows : ''} rows={type === 'textarea'? rows : ''} type={type} placeholder={placeholder}></Ipt>
     </React.Fragment>
   )
 };
 
-Button.defaultProps = {
-  text: '버튼',
-  backgroundColor: null,
+Input.defaultProps = {
+  type: 'text',
+  placeholder: null,
   color: null,
   fontSize: null,
-  fontWeight: null,
   border: null,
   width: null,
   height: null,
@@ -37,20 +36,19 @@ Button.defaultProps = {
   padding: null,
   top: null,
   left: null,
+
 }
 
-const Btn = styled.button`
-  background-color: ${(props) => props.backgroundColor};
+const Ipt = styled.input`
   color: ${(props) => props.color};
   font-size: ${(props) => props.fontSize};
-  font-weight: ${(props) => props.fontWeight};
   border: ${(props) => props.border};
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-  margin: ${(props) => props.margin}; 
+  margin: ${(props) => props.margin};
   padding: ${(props) => props.padding};
   top: ${(props) => props.top};
   left: ${(props) => props.left};
 `;
 
-export default Button;
+export default Input;
