fa5e3d7809ee6b8462b47de5317253812eb7009c
leedmeen <leedmeen@gmail.com>
2021-07-13 20:01:09 +0900

    로그인 페이지 버튼 변경

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 import Grid from "../element/Grid";
 import Input from "../element/Input";
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import Button from '../element/Button';
 
 
 const Login = (props) => {
@@ -18,28 +18,24 @@ const Login = (props) => {
             transitionLeave={false}>   
             <Header/>
         <Grid display="flex" fd="column" mh="100vh" is_center>
-            <Grid br="5px" width="70vw" dp="inline-block" box_shadow="0 4px 25px 0px rgb(0 0 0 / 15%)" padding="20px">
+            <Grid br="5px" width="70vw" dp="inline-block" box_shadow="0 4px 25px 0px rgb(0 0 0 / 15%)" padding="5vh 4vw 7vh 4vw">
             <Grid dp="flex" margin="0 0 20px 0">
-                <Grid width="30vw" padding="25px 10px 0 0" is_right>아이디 :</Grid>
-                <Grid width="70vw" padding="0 30px 0 0">
+                <Grid width="15vw" padding="25px 10px 0 0" is_center>아이디 :</Grid>
+                <Grid width="85vw" padding="0 30px 0 0">
                 <TextField fullWidth="True" id="standard-basic" color="secondary" label="Id" />
                 </Grid>
             </Grid>
             <Grid dp="flex">
-                <Grid width="30vw" padding="25px 10px 0 0" is_right>비밀번호 : </Grid>
-                <Grid width="70vw" padding="0 30px 0 0">
+                <Grid width="15vw" padding="25px 10px 0 0" is_center>비밀번호 : </Grid>
+                <Grid width="85vw" padding="0 30px 0 0">
                 <TextField fullWidth="True" id="standard-basic" color="secondary" label="Password" />
                 </Grid>
             </Grid>
-            <Grid margin="30px 0 0 0" padding="0 40px 0 40px">
-            <Button fullWidth="True" variant="contained" color="secondary">
-                로그인
-            </Button>
+            <Grid margin="5vh 0 0 0" padding="0 40px 0 40px">
+            <Button text='로그인' backgroundColor='#e8344e' border='none' color='white' width='100%' height='4vh' padding='0.5vh 1vw 0.5vh 1vw' borderRadius='0.5vh' />
             </Grid>
-            <Grid margin="20px 0 0 0" padding="0 40px 0 40px">
-            <Button fullWidth="True" variant="contained" color="secondary">
-                회원가입
-            </Button>
+            <Grid margin="2vh 0 0 0" padding="0 40px 0 40px">
+            <Button text='회원가입' backgroundColor='#e8344e' border='none' color='white' width='100%' height='4vh' padding='0.5vh 1vw 0.5vh 1vw' borderRadius='0.5vh' />
             </Grid>
             </Grid>
         </Grid>
