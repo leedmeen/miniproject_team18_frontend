89662b7eb4fbb6f90ad57978b2d769de1022a602
leedmeen <leedmeen@gmail.com>
2021-07-13 19:23:50 +0900

    Update CommentList.js

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState} from 'react';
 import Text from '../element/Text';
 import Button from '../element/Button';
 import styled from 'styled-components';
@@ -8,13 +8,10 @@ import { actionCreators as commentActions } from '../redux/modules/comment';
 
 const CommentList = (props) => {
   const dispatch = useDispatch();
-  // console.log(props);
 
   const deleteComment = (adId, id) => {
     dispatch(commentActions.deleteCommentDB(adId, id));
   }
-
-  
 
   return (
     <React.Fragment>
@@ -24,7 +21,7 @@ const CommentList = (props) => {
           <Div><Text color='#E8344E' bold>르탄이 <img src={spartalt} style={{width: '2vw'}}/></Text></Div>
           <Text bold margin='0 1vw 0 0'>{c.content}</Text>
           <Text margin='0 1vw 0 0'>{c.createdAt}</Text>
-          <Button width='60px' height='3vh' backgroundColor='#E8344E' color='white' border='none' borderTLRadius='1vh' borderBLRadius='1vh' fontWeight='bold' text='수정' />
+          <Button _onClick={() => } width='60px' height='3vh' backgroundColor='#E8344E' color='white' border='none' borderTLRadius='1vh' borderBLRadius='1vh' fontWeight='bold' text='수정' />
             <Button _onClick={() => deleteComment(c.adId, c.id)} width='60px' height='3vh' color='white' border='none' borderTRRadius='1vh' borderBRRadius='1vh' fontWeight='bold' backgroundColor='#E8344E' margin='0 0 0 0.2vw' text='삭제' />
       </Box>
         )
